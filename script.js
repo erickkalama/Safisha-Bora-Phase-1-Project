6491bb52ed9387a18860046899f79bbac92c4c6e
GHOSTFACE <erickkatana4787@gmail.com>
2024-07-19 08:45:41 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Event listeners for authentication buttons
+    document.getElementById("login-button").addEventListener("click", () => {
+        window.location.href = 'login.html';
+    });
+
+    document.getElementById("signup-button").addEventListener("click", () => {
+        window.location.href = 'signup.html';
+    });
+
+    // Event listeners for tab links
+    var tabLinks = document.querySelectorAll(".tab-links");
+    var tabContents = document.querySelectorAll(".tab-contents");
+
+    tabLinks.forEach(tabLink => {
+        tabLink.addEventListener("click", (event) => {
+            openTab(event.currentTarget.getAttribute("data-tab"));
+        });
+    });
+
+    // Fetch services when the page loads
+    fetchServices();
+
+    function fetchServices() {
+        fetch("http://localhost:3000/SafishaBora")
+        // Make a GET request to the server
+            .then(response => response.json())
+          //  Parse the JSON from the response
+            .then(services => displayServices(services))
+            // Call displayServices with the parsed data
+            .catch(error => console.error("Error fetching services:", error));
+    }
+
+    function displayServices(services) {
+        const servicesContainer = document.getElementById('services');
+        services.forEach(service => {
+            const serviceElement = document.createElement('div');
+            serviceElement.innerHTML = `<h2>${service.name}</h2><p>${service.description}</p>`;
+            servicesContainer.appendChild(serviceElement);
+        });
+    }
+
+    function openTab(tabName) {
+        tabLinks.forEach(tabLink => {
+            tabLink.classList.remove("active-link");
+        });
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("active-tab");
+        });
+        document.querySelector(`[data-tab="${tabName}"]`).classList.add("active-link");
+        document.getElementById(tabName).classList.add("active-tab");
+    }
+});
